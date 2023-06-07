94c21e12818c78875bf04717529b7e3d272d8421
Howard Tran <anhkhoat03207@gmail.com>
2023-06-07 09:06:40 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Awesome Notion Site
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+This website uses [notion-api-worker](https://github.com/splitbee/notion-api-worker) to fetch data from Notion. It will fetch categories and mocks data first, then open the server (use 2 files fetchMocks.js & fetchConfig.js)
---
+++ fetchConfig.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const request = require('request')
+const path = require('path')
+const categoriesDatabaseId = '741a0da234f64a7e8404ae8d433b26e4'
+
+const envFile = fs.readFileSync(path.join(__dirname, '/.env'), 'utf-8')
+const env = {}
+
+envFile.split('\n').forEach((envData) => {
+  const envVar = envData.split('=')
+  const envName = envVar[0]
+  const envValue = envVar[1]
+  env[[envName]] = envValue
+})
+const WORKER_ENDPOINT = env.WORKER_ENDPOINT
+request
+  .get(`${WORKER_ENDPOINT}/v1/table/${categoriesDatabaseId}`)
+  .pipe(fs.createWriteStream(path.join(__dirname, './src/store/categories.json')))
